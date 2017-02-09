50bfb5d55955246e83abd987098405a8340f560d
K2155 <K2155@OHJE-330-WS-22>
2017-02-09 10:45:15 +0200

    Teht01

---
+++ Teht01/Persons.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht01
+{
+    class Persons 
+
+    {
+        // persons collection stores person objects
+        private List<Person> persons;
+
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Add persons to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        public Person GetPersons(int index)
+        {
+            if (index < persons.Count && index >= 0) return persons.ElementAt(index);
+            else return null;
+        }
+
+        public Person FindPerson(string socialsecuritynumber)
+        {
+            foreach(Person person in persons)
+            {
+                // compare two strings
+                if (socialsecuritynumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            return null; // person searched not in collection
+        }
+        public void PrintData()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,48 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+            // create Persons (collection)
+            Persons myFriends = new Persons();
+
+            // create few Person objects
+
+            Person person1 = new Person { Firstname = "Pete", Lastname = "Kauppine", SocialSecurityNumber = "123456-666A" };
+            Person person2 = new Person { Firstname = "Mara", Lastname = "Raksaukko", SocialSecurityNumber = "654321-1234" };
+            Person person3 = new Person { Firstname = "Pena", Lastname = "Koira", SocialSecurityNumber = "235323-2542" };
+
+            // add Person objects to Persons (myFriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // get one person
+            Person person4 = myFriends.GetPersons(0);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("There is no person at that position!");
+            }
+            Console.WriteLine();
+
+            // print collection
+            myFriends.PrintData();
+
+            // find person
+            string socialsecuritynumber = "123456-666A";
+            Console.WriteLine("Find person with social security number: " + socialsecuritynumber);
+            Person person5 = myFriends.FindPerson(socialsecuritynumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Can't find person with that social security number");
+            }
+
         }
     }
 }
